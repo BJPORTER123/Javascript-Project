01c535f9e79ccce9f069fe1cdfa1043f495f57e0
Ben Barlow <ben.barlow@yahoo.co.uk>
2023-02-08 14:17:50 +0000

    visited button working

--- client/src/components/CountryDetail.js
+++ client/src/components/CountryDetail.js
@@ -5,56 +5,49 @@ import { useParams } from "react-router-dom";
 
 const CountryDetail = ({ removeBucketCountry, selectedCountry, addToBucket, addToVisited, bucketList, visitedList }) => {
 
-    const {countryId} = useParams()
+    const { countryId } = useParams()
 
     const onBucketClick = () => {
-        if(bucketList.filter(country => country.cca2 === selectedCountry.cca2).length === 0) {
+        if (bucketList.filter(country => country.cca2 === selectedCountry.cca2).length === 0) {
             postBucketCountry(selectedCountry)
-            .then(()=>{
-            addToBucket(selectedCountry)
-        })
+                .then(() => {
+                    addToBucket(selectedCountry)
+                })
         }
     }
 
     const onVisitedClick = () => {
-        if(visitedList.filter(country => country.cca2 === selectedCountry.cca2).length === 0 && bucketList.filter(country => country.cca2 === selectedCountry.cca2).length === 0) {
+        if (visitedList.filter(country => country.cca2 === selectedCountry.cca2).length === 0 && bucketList.filter(country => country.cca2 === selectedCountry.cca2).length === 0) {
             postVisitedCountry(selectedCountry)
-            .then((response)=>{
-                const copyOfSelectedCountry = {...selectedCountry}
-                copyOfSelectedCountry._id = response.insertedId
-                addToVisited(copyOfSelectedCountry)
-                deleteBucketCountry(countryId)
-                    .then(() => {
-                    removeBucketCountry(countryId)
-            })
-        })
+                .then((response) => {
+                    const copyOfSelectedCountry = { ...selectedCountry }
+                    copyOfSelectedCountry._id = response.insertedId
+                    addToVisited(copyOfSelectedCountry)
+                    deleteBucketCountry(countryId)
+                        .then(() => {
+                            removeBucketCountry(countryId)
+                        })
+                })
         }
     }
 
     return (
         <>
-            <NavBar/>
+            <NavBar />
             <div>
-            <img src={selectedCountry.flags.png} alt={selectedCountry.flags.alt}/><br>
+                <img src={selectedCountry.flags.png} alt={selectedCountry.flags.alt} /><br>
                 </br>
                 {selectedCountry.name.common} <br>
                 </br>
-                {selectedCountry.capital} 
-                
+                {selectedCountry.capital}
             </div>
             {visitedList.filter(country => country.cca2 === countryId).length === 0 && bucketList.filter(country => country.cca2 === countryId).length === 0
                 ? <div>
-                    <button type="Submit" value='add-to-bucket' onClick={onBucketClick} >Add Bucket</button> 
+                    <button type="Submit" value='add-to-bucket' onClick={onBucketClick} >Add Bucket</button>
                     <button type="Submit" value='add-to-visited' onClick={onVisitedClick}>Add Visited</button>
-                </div> 
+                </div>
                 : "Added to list!"
-
- 
-
             }
-        
-            
-            
         </>
     );
 }
--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -3,7 +3,7 @@ import NavBar from '../components/NavBar';
 import SearchBar from '../components/SearchBar';
 
 
-const MainContainer = ({ countries, onCountryClicked, onSubmitSearch, addToVisited, error, searchedCountries }) => {
+const MainContainer = ({ countries, onCountryClicked, onSubmitSearch, addToVisited, error, searchedCountries, visitedList, onBucketClick, onVisitedClick}) => {
 
     return (
         <>
@@ -16,13 +16,13 @@ const MainContainer = ({ countries, onCountryClicked, onSubmitSearch, addToVisit
                     </>
                     :
                     <>
-                        <CountryList countriesSlice={searchedCountries} countries={countries} onCountryClicked={onCountryClicked} addToVisited={addToVisited} />
+                        <CountryList countriesSlice={searchedCountries} countries={countries} onCountryClicked={onCountryClicked} addToVisited={addToVisited} visitedList={visitedList} onBucketClick={onBucketClick} onVisitedClick={onVisitedClick}/>
                     </>
                 }
             </div>
             <div>
                 <h3 className="list-title">Countries Suggestions</h3>
-                <CountryList countriesSlice={countries.slice(0, 20)} countries={countries} onCountryClicked={onCountryClicked} addToVisited={addToVisited} />
+                <CountryList countriesSlice={countries.slice(0, 20)} countries={countries} onCountryClicked={onCountryClicked} addToVisited={addToVisited} visitedList={visitedList} onBucketClick={onBucketClick} onVisitedClick={onVisitedClick}/>
             </div>
         </>
 
